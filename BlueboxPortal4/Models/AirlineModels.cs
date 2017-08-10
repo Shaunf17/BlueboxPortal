88334f83fb3f5701a83bb21dbcd401acb8b6b836
Shaun Falconer <sfalconer@blueboxaviation.com>
2017-08-10 15:13:47 +0100

    Airline Models Working

---
+++ BlueboxPortal4/Models/AirlineModels.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlueboxPortal4.Models
+{
+    public class Airline
+    {
+        [Key]
+        public string Id { get; set; }
+
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+
+        [Display(Name = "Name Alias")]
+        public string NameAlias { get; set; }
+
+        [Display(Name = "Friendly Name")]
+        public string FriendlyName { get; set; }
+
+        [Display(Name = "Display Name")]
+        public string DisplayName { get; set; }
+
+        [Display(Name = "Database Name")]
+        public string DbName { get; set; }
+
+        [Display(Name = "SSRS Folder")]
+        public string SSRSFolder { get; set; }
+
+        public virtual ICollection<ApplicationUser> ApplicationUser { get; set; }
+
+        public IEnumerable<Airline> getAirlines
+        {
+            get { return getAirlines.OrderBy(a => a.Name); }
+        }
+    }
+
+    public class AirlineContext : DbContext
+    {
+        public AirlineContext() : base("DefualtConnection")
+        {
+        }
+
+        public DbSet<Airline> Airline { get; set; }
+    }
+}
--- BlueboxPortal4/Models/IdentityModels.cs
+++ BlueboxPortal4/Models/IdentityModels.cs
@@ -3,6 +3,7 @@ using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System.Collections.Generic;
 
 namespace BlueboxPortal4.Models
 {
@@ -16,6 +17,8 @@ namespace BlueboxPortal4.Models
             // Add custom user claims here
             return userIdentity;
         }
+        public virtual ICollection<Airline> Airline { get; set; }
+
     }
 
     public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
@@ -29,5 +32,7 @@ namespace BlueboxPortal4.Models
         {
             return new ApplicationDbContext();
         }
+
+        //public System.Data.Entity.DbSet<BlueboxPortal4.Models.ApplicationUser> ApplicationUsers { get; set; }
     }
 }
