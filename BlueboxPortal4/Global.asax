c56f3cd062dfb6524c905db2809274c1f92fed9a
Shaun Falconer <sfalconer@aviit.com>
2017-08-10 09:09:48 +0100

    First Commit

---
+++ BlueboxPortal4/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlueboxPortal4.MvcApplication" Language="C#" %>
---
+++ BlueboxPortal4/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlueboxPortal4.Startup))]
+namespace BlueboxPortal4
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
